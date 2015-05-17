eaf7e7b12649c76840008f26b783eef8d3401cd4
jeznag <jeremymnagel@gmail.com>
2015-05-17 15:06:25 +1000

    Fix indentation in person serializer

--- app/serializers/person.js
+++ app/serializers/person.js
@@ -4,46 +4,44 @@ import config from '../config/environment';
 var counter = 1;
 export default DS.RESTSerializer.extend({
 
-	extractSingle: function(store, type, payload, id) {
-	    var people = payload.results.people;
-	    delete payload.results;
-	    delete payload.name;
-	    delete payload.count;
+    extractSingle: function(store, type, payload, id) {
+        var people = payload.results.people;
 
-	    payload = { people: people };
-	    return this._super(store, type, payload, id);
+        payload = {
+            people: people
+        };
+        return this._super(store, type, payload, id);
     },
 
     extractArray: function(store, type, payload) {
-		if (config.environment !== 'production'){
-			//using Mirage
-			return this._super(store, type, payload);
-		}
+        if (config.environment !== 'production') {
+            //using Mirage
+            return this._super(store, type, payload);
+        }
 
-	    var people = payload.results.people;
-	    delete payload.results;
-	    delete payload.name;
-	    delete payload.count;
+        var people = payload.results.people;
+        payload = {
+            people: people
+        };
+        return this._super(store, type, payload);
+    },
 
-	    payload = { people: people };
-	    return this._super(store, type, payload);
-	},
+    normalizeHash: {
+        // Next, normalize individual people, which (after `extract`)
+        // are now located under `people`
+        people: function(hash) {
+            if (config.environment !== 'production') {
+                //using Mirage - no normalisation necessary
+                return;
+            }
+            //fix variable names as the model uses fullName and photoURL rather than name/photo
+            hash.fullName = hash.name;
+            hash.photoURL = hash.photo;
+            hash.id = counter;
+            counter++;
 
-  	normalizeHash: {
-	    // Next, normalize individual people, which (after `extract`)
-	    // are now located under `people`
-	    people: function(hash) {
-	      if (config.environment !== 'production'){
-				//using Mirage
-				return;
-		  }
-	      hash.fullName = hash.name;
-	      hash.photoURL = hash.photo;
-	      hash.id = counter; 
-	      counter++;
-
-	      delete hash.name;
-	      delete hash.photo;
-	    }
-	}
+            delete hash.name;
+            delete hash.photo;
+        }
+    }
 });
